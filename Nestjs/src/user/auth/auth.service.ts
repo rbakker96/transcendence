5c4a6d24b1af8252e6cd03c7567bac4e2f43e50e
Roy Bakker <57988535+rbakker96@users.noreply.github.com>
2021-07-05 21:05:07 +0200

    almost there I hope

--- Nestjs/src/user/auth/auth.service.ts
+++ Nestjs/src/user/auth/auth.service.ts
@@ -1,9 +1,13 @@
-import {Body, Injectable} from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Injectable } from '@nestjs/common';
+import { authenticator } from "otplib";
+import { Request, Response } from 'express';
 import { JwtService } from "@nestjs/jwt";
 import { RegisterDto } from "./models/register.dto";
 import { UserService} from "../user.service";
 import { UpdateDto } from "./models/update.dto";
+import QRCode from 'qrcode'
+import { toString } from 'qrcode';
+// import { toDataURL } from 'qrcode';
 
 @Injectable()
 export class AuthService {
@@ -11,6 +15,40 @@ export class AuthService {
         private jwtService: JwtService,
         private userService: UserService,
     ) {}
+
+    async twoFactorAuthSecret(clientID: number) {
+        const client = await this.userService.findOne(clientID);
+        console.log(client);
+        const secret = authenticator.generateSecret();
+        console.log(secret);
+        await this.userService.saveTwoFactorSecret(secret, clientID);
+
+
+        const updated_client = await this.userService.findOne(clientID);
+        console.log(updated_client);
+
+
+        return  authenticator.keyuri(client.email, 'ft_transcendence', secret); //OtpAuthUrl
+    }
+
+    async createQRcode(stream: Response, otpauthUrl: string) {
+        var QRCode = require('qrcode');
+
+        console.log("here");
+        await QRCode.toFile('./qrcode.png', otpauthUrl);
+        console.log('not here');
+        return 'success';
+        // stream.setHeader("content-type","image/png");
+        // return toFileStream(stream, otpauthUrl);
+        // console.log(await QRCode.toDataURL(otpauthUrl))
+        // console.log( await QRCode.toDataURL(otpauthUrl));
+    }
+
+    async twoFactorAuthVerify(code: string, clientID: number) {
+        const client = await this.userService.findOne(clientID);
+
+        return authenticator.verify({token: code, secret: client.twoFactorSecret});
+    }
 
     async clientID(request: Request): Promise<number> {
         const cookie = request.cookies['clientID'];
