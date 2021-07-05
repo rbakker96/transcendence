46d27820885bfa82493dc3bbedb481e285e1a76c
qingqingqingli <kadlqbetty@gmail.com>
2021-07-05 17:13:59 +0200

    Save filled text bar data in state

--- React/src/components/chat/ChatInputBar.tsx
+++ React/src/components/chat/ChatInputBar.tsx
@@ -1,17 +1,37 @@
 import styles from "./ChatInputBar.module.css";
-import TextArea from "antd/lib/input/TextArea";
+import { useState } from "react";
+// import TextArea from "antd/lib/input/TextArea";
 
 type TextBarType = {
   activeChannelID: number;
 };
 
 function ChatInputBar(props: TextBarType) {
+  const [inputValue, setInputValue] = useState("");
+
+  function submitHandler() {
+    console.log("submitted value: " + inputValue);
+  }
+
+  function changeHandler(event: any) {
+    setInputValue(event.target.value);
+  }
+
   if (props.activeChannelID) {
     return (
       <div className={styles.textBar}>
-        <TextArea autoSize allowClear={true} placeholder={"Enter message..."} />
-        {/*<input className={styles.textBarInput} type="text" placeholder={"Enter message..."} />*/}
-        <button className={styles.textBarSend} type={"submit"}>
+        {/*<TextArea autoSize allowClear={true} placeholder={"Enter message..."} />*/}
+        <input
+          className={styles.textBarInput}
+          type="text"
+          placeholder={"Enter message..."}
+          onChange={changeHandler}
+        />
+        <button
+          className={styles.textBarSend}
+          type={"submit"}
+          onClick={submitHandler}
+        >
           Send
         </button>
       </div>
