b3660141b98eb9bea28ff2b3f9bdf8b92ef04c32
Roy Bakker <57988535+rbakker96@users.noreply.github.com>
2021-07-08 15:03:24 +0200

    created backend files for database handeling game component

---
+++ Nestjs/src/game/game.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
+import { UserService } from "../user/user.service";
+import { AuthService } from "../user/auth/auth.service";
+import { Request } from "express";
+import { verifyUser } from "../user/auth/strategy/auth.guard";
+import { newGameDto } from "./models/newGame.dto";
+import { GameService } from "./game.service";
+import { gameStatsDto } from "./models/gameStats.dto";
+
+
+@Controller()
+export class AuthController {
+    constructor(
+        private gameService: GameService,
+        private userService: UserService,
+        private authService: AuthService
+    ) {}
+
+
+    @UseGuards(verifyUser)
+    @Get('gameData')
+    async gameData (@Req() request: Request, @Body() gameID) : Promise<any> {
+        return this.gameService.findOne(gameID);
+    }
+
+    @UseGuards(verifyUser)
+    @Post('newGame')
+    async newGame (@Req() request: Request, @Body() data: newGameDto) : Promise<any> {
+        return this.gameService.create(data);
+    }
+
+    @UseGuards(verifyUser)
+    @Post('updateGameStats')
+    async updateGameStats (@Req() request: Request, @Body() data: gameStatsDto) : Promise<any> {
+        return this.gameService.update(data.gameID, data);
+    }
+}
--- Nestjs/src/game/game.module.ts
+++ Nestjs/src/game/game.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { GameGateway } from './game.gateway';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Game } from "./game.entity";
+import { GameService } from "./game.service";
+import { AuthController } from "../user/auth/auth.controller";
+import { AuthService } from "../user/auth/auth.service";
 
 @Module({
-	providers: [GameGateway]
+	imports: [
+		TypeOrmModule.forFeature([Game]),
+	],
+	controllers: [AuthController, AuthService],
+	providers: [GameGateway, GameService]
 })
 export class GameModule {}
