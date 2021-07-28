18f6aa8f126f2b3b7671be57d64bf2e2c1ff8657
gbouwen <gijsbouwen@gmail.com>
2021-07-28 12:54:51 +0200

    Added leaveWaitingRoom() so if a player leaves the waiting room it will be removed from the waitingUsers[]

--- React/src/components/game/WaitingRoom.tsx
+++ React/src/components/game/WaitingRoom.tsx
@@ -101,6 +101,18 @@ const WaitingRoom = (props: any) => {
         };
     }, [user, props.location.state]);
 
+    useEffect(() => {
+        let mounted = true;
+
+        window.onpopstate = () => {
+            websocket.current.send(JSON.stringify({ event: 'leaveWaitingRoom'}));
+        }
+        window.onbeforeunload = () => {
+            websocket.current.send(JSON.stringify({ event: 'leaveWaitingRoom'}));
+        }
+        return () => {mounted = false;}
+    }, []);
+
 
     if (unauthorized)
         return <Redirect to={'/'} />;
